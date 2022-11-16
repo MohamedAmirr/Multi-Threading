ba43d5f125273f027c3a01cb411f1837d24028ae
Mohamed Amir <m.elamir027@gmail.com>
2022-11-16 09:00:04 +0200

    classes and GUI added

---
+++ src/Consumer.java
@@ -0,0 +1,23 @@
+package src;
+
+import java.util.Queue;
+
+public class Consumer extends Thread {
+
+    public void run(Object obj) throws Exception{
+        synchronized (obj) {
+            while(true){
+                Producer producer = new Producer();
+                if(producer.getBool())
+                    break;
+                Queue<Integer> q1 = producer.getQ();
+                while (q1.size() > 0) {
+                    System.out.println(q1.peek());
+                    q1.remove();
+                }
+                obj.notify();
+                obj.wait();
+            }
+        }
+    }
+}
---
+++ src/GUIForm.java
@@ -0,0 +1,8 @@
+package src;
+
+import javax.swing.*;
+
+public class GUIForm {
+    private JTextArea generatePrimeNumbersTextArea;
+    private JTextField textField1;
+}
